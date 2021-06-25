dff5e71676498da185b653d90c6a6564b2e6d86c
cesar acosta <ceagdev@gmail.com>
2021-06-25 01:16:07 -0400

    LetterList fragment implemented

---
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WordListFragment.new Instance] factory method to
+ * create an instance of this fragment.
+ */
+class WordListFragment : Fragment() {
+
+}
